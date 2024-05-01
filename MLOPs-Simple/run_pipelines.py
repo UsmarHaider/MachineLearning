a02a1f58ac79d330f20821de046dcafc743ceb0d
UsmarHaider <125660169+UsmarHaider@users.noreply.github.com>
2024-05-01 14:39:38 +0500

    mlop delete

--- MLOPs-Simple/run_pipelines.py
+++
@@ -1,4 +0,0 @@
-from pipelines.training_pipeline import train_pipeline
-
-if __name__ == "__main__":
-    train_pipeline(data_path='D:\MLOPs\pythonProject\data\olist_customers_dataset.csv')
--- MLOPs-Simple/steps/config.py
+++
@@ -1,4 +0,0 @@
-from zenml.steps import BaseParameters
-
-class ModelNameConfig(BaseParameters):
-    model_name: str = "LinearRegressionModel"
